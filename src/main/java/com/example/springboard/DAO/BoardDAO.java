c76837963cb972fa4e938a08ac9d69d8d11c3f44
GPlogger <kkjj4414@naver.com>
2022-11-05 21:17:09 +0900

    DAO 삭제

--- src/main/java/com/example/springboard/DAO/BoardDAO.java
+++
@@ -1,9 +0,0 @@
-package com.example.springboard.DAO;
-
-import com.example.springboard.entity.BoardEntity;
-
-public class BoardDAO {
-    BoardEntity saveBoard(BoardEntity boardEntity);
-
-    BoardEntity getBoard(Long id);
-}
